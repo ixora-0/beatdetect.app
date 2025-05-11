7b08a4f316869e9c44339e8c0415891283d95e34
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2025-05-11 06:19:04 -0400

    scripts: Add create spectral flux script.

--- beatdetect/__init__.py
+++ beatdetect/__init__.py
@@ -10,6 +10,7 @@ ANNOTATIONS_GITHUB_BRANCH = "main"
 ANNOTATIONS_RAW_PATH = pathlib.Path("data/raw/annotations")
 ANNOTATIONS_PROCESSED_PATH = pathlib.Path("data/processed/annotations")
 ONE_HOT_BEATS_PATH = pathlib.Path("data/processed/one-hot-annotations/")
+SPECTRAL_FLUX_PATH = pathlib.Path("data/processed/spectral-flux/")
 
 SPECTROGRAMS_URL_TEMPLATE = string.Template(
     "https://zenodo.org/records/13922116/files/${dataset}.zip?download=1"
---
+++ beatdetect/scripts/create_spectral_flux.py
@@ -0,0 +1,60 @@
+import argparse
+
+import librosa
+import numpy as np
+import torch
+import torchaudio
+
+from beatdetect import (
+    DATASETS,
+    SPECTRAL_FLUX_PATH,
+    SPECTROGRAMS_RAW_PATH,
+)
+from beatdetect.histogram_params import HOP_LENGTH, SAMPLE_RATE
+
+
+def main(specified_dataset=None):
+    print(f"Saving spectral flux files into {SPECTRAL_FLUX_PATH}")
+    count = 0
+    for dataset in DATASETS if specified_dataset is None else [specified_dataset]:
+        spectrograms = np.load(SPECTROGRAMS_RAW_PATH / dataset / f"{dataset}.npz")
+        spectral_flux_dir = SPECTRAL_FLUX_PATH / dataset
+        spectral_flux_dir.mkdir(parents=True, exist_ok=True)
+        for file_name in spectrograms:
+            if not file_name.endswith("/track"):
+                continue
+            print(f"\033[KProcessing file {file_name}", end="\r")
+            name = file_name.removesuffix("/track")
+
+            melspect = torch.from_numpy(spectrograms[file_name]).to(torch.float32)
+            # ibrosa.onset.onset_strength requires log-power spectrogram
+            log_melspect = torchaudio.transforms.AmplitudeToDB(stype="power")(melspect)
+            spectral_flux = librosa.onset.onset_strength(
+                S=log_melspect.T,
+                sr=SAMPLE_RATE,
+                hop_length=HOP_LENGTH,
+                lag=2,
+                max_size=3,
+            )
+
+            torch.save(
+                torch.from_numpy(spectral_flux), spectral_flux_dir / f"{name}.pt"
+            )
+            count += 1
+
+    print()
+    print(f"Finished processing {count} files")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=f"Generate spectral flux from spectrograms, and save them to {SPECTRAL_FLUX_PATH}."
+    )
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        help="Name of the dataset. If omitted, create spectral flux for all datasets.",
+    )
+    args = parser.parse_args()
+
+    main(args.dataset)
