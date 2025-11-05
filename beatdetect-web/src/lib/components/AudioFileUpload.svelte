7cbd27c3e0e0ec3e5b2b83ce8d6cd910c5bda238
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2025-11-04 19:17:15 -0500

    feat(web): Add audio upload.

---
+++ beatdetect-web/src/lib/components/AudioFileUpload.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import IconFileAudioRegular from 'phosphor-icons-svelte/IconFileAudioRegular.svelte';
+  import IconTrashBold from 'phosphor-icons-svelte/IconTrashBold.svelte';
+  import IconWarningRegular from 'phosphor-icons-svelte/IconWarningRegular.svelte';
+
+  import { FileUpload } from '@skeletonlabs/skeleton-svelte';
+  import { Toast, createToaster } from '@skeletonlabs/skeleton-svelte';
+
+  const toaster = createToaster({
+    placement: 'top-end'
+  });
+  interface Props {
+    onFileUploaded?: (file: File) => void;
+  }
+  let { onFileUploaded }: Props = $props();
+
+  let uploadedFile: File | null = $state(null);
+  function onFileAccept(details: { files: File[] }) {
+    if (details.files.length > 0) {
+      onFileUploaded?.(details.files[0]);
+      uploadedFile = details.files[0];
+    }
+  }
+  function onFileReject(details: { files: { file: File; errors: string[] }[] }) {
+    toaster.error({
+      title: `Cannot process ${details.files[0].file.name}`,
+      description: details.files[0].errors
+    });
+  }
+  function clearFile() {
+    uploadedFile = null;
+  }
+</script>
+
+{#if uploadedFile}
+  <div class="flex items-center gap-4 rounded border p-4">
+    <IconFileAudioRegular class="size-6" />
+    <span>File uploaded: {uploadedFile.name}</span>
+    <button
+      type="button"
+      onclick={clearFile}
+      class="btn-icon preset-outlined transition-all hover:preset-filled-error-500 hover:shadow-lg hover:shadow-error-500/50"
+      title="Clear file"
+      aria-label="Clear file"
+    >
+      <IconTrashBold class="size-6" />
+    </button>
+  </div>
+{:else}
+  <FileUpload accept="audio/*" maxFiles={1} {onFileAccept} {onFileReject}>
+    <FileUpload.Dropzone>
+      <IconFileAudioRegular class="size-10" />
+      <span>Select file or drag here.</span>
+      <FileUpload.Trigger>Browse Files</FileUpload.Trigger>
+      <FileUpload.HiddenInput />
+    </FileUpload.Dropzone>
+  </FileUpload>
+{/if}
+
+<Toast.Group {toaster}>
+  {#snippet children(toast)}
+    <Toast {toast}>
+      <IconWarningRegular class="size-8" />
+      <Toast.Message>
+        <Toast.Title class="flex items-center gap-2">{toast.title}</Toast.Title>
+        <Toast.Description>{toast.description}</Toast.Description>
+      </Toast.Message>
+      <Toast.CloseTrigger />
+    </Toast>
+  {/snippet}
+</Toast.Group>
--- beatdetect-web/src/routes/+page.svelte
+++ beatdetect-web/src/routes/+page.svelte
@@ -1,6 +1,36 @@
 <script lang="ts">
+  import AudioFileUpload from '$lib/components/AudioFileUpload.svelte';
   import LightSwitch from '$lib/components/LightSwitch.svelte';
+  import { Progress } from '@skeletonlabs/skeleton-svelte';
+
+  let progress: number | null = $state(null);
+  async function onFileUploaded(file: File) {
+    progress = 0;
+
+    // transform audio here
+    let start = performance.now();
+    function update(now: number) {
+      progress = (now - start) / 1000;
+      if (progress < 1) {
+        requestAnimationFrame(update);
+      } else {
+        progress = null;
+      }
+    }
+    requestAnimationFrame(update);
+  }
 </script>
 
 <h1>Beat detect app</h1>
 <LightSwitch />
+<AudioFileUpload {onFileUploaded} />
+
+{#if progress !== null}
+  <div class="mt-4">Processing audio...</div>
+  <Progress value={progress * 100}>
+    <Progress.Label>{Math.round(progress * 100)}%</Progress.Label>
+    <Progress.Track>
+      <Progress.Range />
+    </Progress.Track>
+  </Progress>
+{/if}
