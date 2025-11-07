5900609beba37fee5a4cc0bf5e54a0e4e50f0c91
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2025-11-06 20:28:17 -0500

    feat(web): Add visualizing mel spectrogram.

--- beatdetect-web/src/routes/+page.svelte
+++ beatdetect-web/src/routes/+page.svelte
@@ -6,7 +6,11 @@
   import { Progress } from '@skeletonlabs/skeleton-svelte';
   import { Toast, createToaster } from '@skeletonlabs/skeleton-svelte';
   import WaveSurfer from 'wavesurfer.js';
+  import Spectrogram from 'wavesurfer.js/dist/plugins/spectrogram.js';
+  import Timeline from 'wavesurfer.js/dist/plugins/timeline.js';
+  import Zoom from 'wavesurfer.js/dist/plugins/zoom.js';
   import type { PageProps } from './$types';
+  import SpectrogramWorker from './spectrogram-worker.ts?worker';
 
   let { data }: PageProps = $props();
   const { config } = data;
@@ -30,7 +34,12 @@
         container: '#waveform',
         waveColor: '#4F4A85',
         progressColor: '#383351',
-        url: url
+        url: url,
+        minPxPerSec: 20,
+        plugins: [
+          Timeline.create({ height: 25, style: { fontSize: '20px' } }),
+          Zoom.create({ scale: 0.1, maxZoom: 200 })
+        ]
       });
 
       wavesurfer.on('interaction', () => {
@@ -53,29 +62,112 @@
   });
 
   let progress: number | null = $state(null);
-  async function processFile() {
-    progress = 0;
-
-    // transform audio here
-    let start = performance.now();
-    function update(now: number) {
-      progress = (now - start) / 1000;
-      if (progress < 1) {
-        requestAnimationFrame(update);
-      } else {
-        progress = null;
-      }
+  async function calculateSpectrogram(): Promise<number[][] | null> {
+    if (uploadedFile === null) {
+      toaster.error({ title: "Can't process file because it is not loaded." });
+      return null;
     }
-    requestAnimationFrame(update);
+
+    // Read and decode audio
+    const audioContext = new window.AudioContext();
+    const arrayBuffer = await uploadedFile.arrayBuffer();
+    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+
+    // Resample to config's sample_rate if necessary
+    let resampledBuffer = audioBuffer;
+    if (audioBuffer.sampleRate !== config.spectrogram.sample_rate) {
+      const offlineContext = new OfflineAudioContext(
+        audioBuffer.numberOfChannels,
+        audioBuffer.duration * config.spectrogram.sample_rate,
+        config.spectrogram.sample_rate
+      );
+      const source = offlineContext.createBufferSource();
+      source.buffer = audioBuffer;
+      source.connect(offlineContext.destination);
+      source.start();
+      resampledBuffer = await offlineContext.startRendering();
+    }
+
+    // Convert to mono
+    const mono = new Float32Array(resampledBuffer.length);
+    for (let i = 0; i < resampledBuffer.length; i++) {
+      let sum = 0;
+      // Average channels
+      for (let channel = 0; channel < resampledBuffer.numberOfChannels; channel++) {
+        sum += resampledBuffer.getChannelData(channel)[i];
+      }
+      mono[i] = sum / resampledBuffer.numberOfChannels;
+    }
+
+    const worker = new SpectrogramWorker();
+    return new Promise((resolve, reject) => {
+      worker.onmessage = (e) => {
+        if (e.data.type == 'progress') {
+          progress = e.data.progress;
+        } else if (e.data.type == 'complete') {
+          progress = null;
+          const { melSpect } = e.data;
+          worker.terminate();
+          resolve(melSpect);
+        }
+      };
+
+      worker.onerror = (e) => {
+        worker.terminate();
+        toaster.error({ title: 'Error while calculating spectrogram.', description: e.error });
+        reject();
+      };
+
+      worker.postMessage({
+        mono: mono,
+        config: config.spectrogram
+      });
+    });
+  }
+
+  async function processFile() {
+    await calculateSpectrogram();
+
+    // HACK: Recalculating spectrogram here for visualization in wavesurfer
+    // Can't get calculated spectrogram from wavesurfer,
+    // and frequenciesDataUrl doesn't work
+    wavesurfer?.registerPlugin(
+      Spectrogram.create({
+        sampleRate: config.spectrogram.sample_rate,
+        // This creates white background
+        // frequencyMax: config.spectrogram.f_max,
+        // frequencyMin: config.spectrogram.f_min,
+        fftSamples: config.spectrogram.n_fft,
+        useWebWorker: true,
+        scale: 'mel',
+        labels: true
+      })
+    );
   }
 </script>
 
 <h1>Beat detect app</h1>
 <LightSwitch />
+<form>
+  <!--Not allowing params to change for now-->
+  <label class="label">
+    <span class="label-text">Sample rate</span>
+    <input class="input" type="number" disabled value={config.spectrogram.sample_rate} />
+  </label>
+  <label class="label">
+    <span class="label-text">Hop size</span>
+    <input class="input" type="number" disabled value={config.spectrogram.hop_length} />
+  </label>
+  <label class="label">
+    <span class="label-text"># mel bands</span>
+    <input class="input" type="number" disabled value={config.spectrogram.n_mels} />
+  </label>
+</form>
+
 {#if uploadedFile !== null}
   <div
     id="waveform"
-    class="h-32 w-full {!isWaveformReady ? 'placeholder animate-pulse' : ''}"
+    class="w-full {!isWaveformReady ? 'h-32 placeholder animate-pulse' : ''}"
   ></div>
 {/if}
 
@@ -93,8 +185,8 @@
 
 {#if progress !== null}
   <div class="mt-4">Processing audio...</div>
-  <Progress value={progress * 100}>
-    <Progress.Label>{Math.round(progress * 100)}%</Progress.Label>
+  <Progress value={progress}>
+    <Progress.Label>{Math.round(progress)}%</Progress.Label>
     <Progress.Track>
       <Progress.Range />
     </Progress.Track>
---
+++ beatdetect-web/src/routes/spectrogram-worker.ts
@@ -0,0 +1,36 @@
+import Meyda from 'meyda';
+
+self.onmessage = function (e) {
+  const { mono, config } = e.data;
+
+  Meyda.bufferSize = config.n_fft;
+  Meyda.sampleRate = config.sample_rate;
+  Meyda.melBands = config.n_mels;
+
+  const melSpect: number[][] = [];
+
+  const end = mono.length - config.n_fft;
+
+  for (let i = 0; i < end; i += config.hop_length) {
+    const window = mono.slice(i, i + config.n_fft);
+
+    const melBands = Meyda.extract('melBands', window);
+    if (melBands === null) {
+      self.postMessage({ error: 'Meyda library returns null.' });
+      return;
+    }
+    melSpect.push(melBands as number[]);
+
+    // Send progress updates
+    self.postMessage({
+      type: 'progress',
+      progress: (i / end) * 100
+    });
+  }
+
+  // Send the final result
+  self.postMessage({
+    type: 'complete',
+    melSpect: melSpect
+  });
+};
