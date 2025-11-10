75795c864446ce836697a185a580558d09ca63bf
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2025-11-10 00:53:04 -0500

    fix(web): Define config path alias in svelte config.

--- beatdetect-web/svelte.config.js
+++ beatdetect-web/svelte.config.js
@@ -1,11 +1,17 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [vitePreprocess(), mdsvex()],
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: adapter(),
+    alias: {
+      '@configs': path.resolve(__dirname, '../configs')
+    }
+  },
   extensions: ['.svelte', '.svx']
 };
 
--- beatdetect-web/vite.config.ts
+++ beatdetect-web/vite.config.ts
@@ -1,18 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
   server: {
     watch: {
       usePolling: true
-    }
-  },
-  resolve: {
-    alias: {
-      '@configs': path.resolve(__dirname, '../configs')
     }
   },
 
